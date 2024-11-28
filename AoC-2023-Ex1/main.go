ceac1fff71b5f225eaaa06f2799d06d8d703608a
archastronaut-hypr <gericapo98@yahoo.com>
2024-11-29 00:37:51 +0100

    still can't find the reason for false output in AoC 1

--- AoC-2023-Ex1/main.go
+++ AoC-2023-Ex1/main.go
@@ -16,11 +16,16 @@ func extractDigits(line string) (int,int){
       }
       lastDigit = ch
     }
-  }
+
+    if firstDigit == 0 || lastDigit == 0{
+      fmt.Println("Error: Line does not contain valid digits:", line)
+      return 0,0
+    } 
   return int(firstDigit - '0'), int(lastDigit - '0')
 }
 
 func combineDigits(first, last int) int {
+  fmt.Println("Combining digits, resulting to: 10*%d + %d = %d", first, last, 10*first + last)
   return 10*first + last
 }
 
@@ -28,10 +33,13 @@ func calculateSum(lines []string) int{
   total := 0
   for _, line := range lines {
     first, last := extractDigits(line)
+    if first == 0 && last == 0 {
+			fmt.Println("Skipping line with no digits:", line)
+			continue
+		}
     calibrationValue := combineDigits(first, last)
     total += calibrationValue
   }
-
   return total
 }
 
